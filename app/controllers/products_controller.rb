7eb144266b33232083c31c3fb626db3ca56f19c6
bcarnevale <bianca.carnevale@hotmail.com>
2018-10-29 14:26:56 +1100

    Image upload feature added for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:shop_profile_id, :name, :price, :style, :description)
+      params.require(:product).permit(:shop_profile_id, :name, :price, :style, :description, :image)
     end
 end
