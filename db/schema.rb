acddb1f6c47bd174d611d2408ea2737da3519b6e
James Merrigan <merrigan.james@gmail.com>
2018-10-29 14:02:09 +1100

    Create ShopProfile model and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_29_024554) do
+ActiveRecord::Schema.define(version: 2018_10_29_025919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shop_profiles", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "business_name"
+    t.text "about"
+    t.string "location"
+    t.string "contact_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_shop_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +38,5 @@ ActiveRecord::Schema.define(version: 2018_10_29_024554) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "shop_profiles", "users"
 end
