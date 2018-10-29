7ada8c77cff206010e956ddd1f086503f4fcdf5d
James Merrigan <merrigan.james@gmail.com>
2018-10-29 14:07:34 +1100

    Scaffolded products

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_29_025919) do
+ActiveRecord::Schema.define(version: 2018_10_29_030546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.bigint "shop_profile_id"
+    t.string "name"
+    t.integer "price"
+    t.string "style"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shop_profile_id"], name: "index_products_on_shop_profile_id"
+  end
 
   create_table "shop_profiles", force: :cascade do |t|
     t.bigint "user_id"
@@ -38,5 +49,6 @@ ActiveRecord::Schema.define(version: 2018_10_29_025919) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "shop_profiles"
   add_foreign_key "shop_profiles", "users"
 end
