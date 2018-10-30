26e29f9e7a0479211c1c80a96421d3b4324081db
bcarnevale <bianca.carnevale@hotmail.com>
2018-10-31 10:08:59 +1100

    Installed Stripe

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :products
   resources :shop_profiles
   devise_for :users
+  resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
