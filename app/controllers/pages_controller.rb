deb882b3ae92002c3d3bd9cdd2b0d9956ceead4a
James Merrigan <merrigan.james@gmail.com>
2018-11-07 13:04:57 +1100

    Added about us page and site footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def welcome
     @products = Product.all
   end
+
+  def about
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#welcome'
+  get '/about', to: 'pages#about'
   get '/profile', to: 'shop_profiles#edit'
   get '/profile/products', to: 'shop_profiles#shop_products'
   get '/profile/orders', to: 'shop_profiles#shop_orders'
